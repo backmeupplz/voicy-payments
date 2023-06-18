08a243048ab0000c7f3790dc86a49cde4cb7e1a4
Nikita Kolmogorov <backmeupplz@gmail.com>
2023-06-17 19:40:57 -0700

    Update db.js

--- helpers/db.js
+++ helpers/db.js
@@ -124,9 +124,9 @@ function getStats() {
 
 async function getNewStats() {
   console.log('getting stats')
-  // Extra numbers of records that don't exist any more after the cleunup but still contribute to the stats
-  const extraVoiceCount = 16472966 + 140155 + 10228067 + 24327202 + 23724547
-  const extraDuration = 185936897 + 2245600 + 147045183 + 344688480 + 7780507 + 352478725
+  // Extra numbers of records that don't exist anymore after the cleanup but still contribute to the stats
+  const extraVoiceCount = 16472966 + 140155 + 10228067 + 24327202 + 23724547 + 48066867
+  const extraDuration = 185936897 + 2245600 + 147045183 + 344688480 + 7780507 + 352478725 + 297266368
   // Get result dummy variable
   const result = {}
   // Get response stats
